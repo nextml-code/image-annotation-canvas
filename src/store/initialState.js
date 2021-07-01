e473712591b8bc388b571276a5a4fd946ae4675b
Filip Johansson <filip.johansson@protonmail.com>
2021-07-01 16:03:13 +0200

    feature!: change bbox format and how they are rendered

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -4,7 +4,6 @@ import { DEFAULT_CURSOR } from "../core/cursorTypes";
 export default Object.freeze({
   activeAnnotationId: null, // id of the currently active annotation
   cursor: DEFAULT_CURSOR,
-  annotations: [], // Array with annotation objects
   mouseIsDown: false,
   resizing: false,
   hoveredPoints: [],
@@ -13,5 +12,10 @@ export default Object.freeze({
   showGhostPolygon: true,
   image: {},
   canvasDimensions: { width: 0, height: 0 },
-  options: {},
+
+  options: {
+    displayBoundingBox: true,
+  },
+
+  annotations: [], // Array with annotation objects
 });
