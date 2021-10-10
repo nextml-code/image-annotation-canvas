9b488def2164b78d8a3cca13a83432dc92122080
Filip Johansson <filip.johansson@protonmail.com>
2021-10-10 21:42:41 +0200

    feature: add configuration options
    make appearance of bbox and polygon
    configurable

--- src/core/drawPolygon.js
+++ src/core/drawPolygon.js
@@ -1,7 +1,11 @@
 import { boundingBoxToAbsolute } from "./boundingBoxToAbsolute";
 import getAbsoluteCoordinate from "./getAbsoluteCoordinate";
 
+<<<<<<< HEAD
 const drawPolygon = (canvas, state, polygon) => {
+=======
+const drawPolygon = (canvas, state, polygon, color) => {
+>>>>>>> 98a94e4 (feature: add configuration options)
   const coordinates = polygon.coordinates.map(
     getAbsoluteCoordinate(state.canvasDimensions),
   );
@@ -10,13 +14,23 @@ const drawPolygon = (canvas, state, polygon) => {
     try {
       const context = canvas.getContext("2d");
 
-      if (state.config.polygon.display) {
+      if (
+        state.config?.polygon?.display === undefined ||
+        state.config?.polygon?.display
+      ) {
         const startCoordinate = coordinates[0];
         context.beginPath();
+<<<<<<< HEAD
         context.fillStyle = `${polygon.color}22`; // eslint-disable-line no-param-reassign
         context.strokeStyle = polygon.color; // eslint-disable-line no-param-reassign, max-len
         context.lineWidth = 4;
         context.setLineDash([]);
+=======
+        context.fillStyle = `${color}22`; // eslint-disable-line no-param-reassign
+        context.strokeStyle = color; // eslint-disable-line no-param-reassign, max-len
+        context.setLineDash(state.config?.polygon?.lineDash || []);
+        context.lineWidth = state.config?.polygon?.lineWidth || 3;
+>>>>>>> 98a94e4 (feature: add configuration options)
         context.moveTo(startCoordinate.x, startCoordinate.y);
         coordinates.slice(1).forEach((coordinate) => {
           context.lineTo(coordinate.x, coordinate.y);
@@ -26,15 +40,21 @@ const drawPolygon = (canvas, state, polygon) => {
         context.stroke();
       }
 
-      if (state.config.bbox.display) {
+      if (state.config?.bbox?.display) {
         const { x, y, width, height } = boundingBoxToAbsolute(
           state.canvasDimensions,
         )(polygon.bbox);
 
         context.beginPath();
+<<<<<<< HEAD
         context.strokeStyle = `${polygon.color}`; // eslint-disable-line no-param-reassign
         context.setLineDash(state.config.bbox.lineDash || []);
         context.lineWidth = state.config.bbox.lineWidth || 3;
+=======
+        context.strokeStyle = `${color}`; // eslint-disable-line no-param-reassign
+        context.setLineDash(state.config?.bbox?.lineDash || []);
+        context.lineWidth = state.config?.bbox?.lineWidth || 3;
+>>>>>>> 98a94e4 (feature: add configuration options)
         context.rect(x, y, width, height);
         context.closePath();
         context.stroke();
