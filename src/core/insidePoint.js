3bc4df46bc11508b772c3ab3a545d624b3f3f14b
Filip Johansson <filip.johansson@protonmail.com>
2021-10-15 12:11:23 +0200

    improve: make grab area bigger for editing points

--- src/core/insidePoint.js
+++ src/core/insidePoint.js
@@ -1,4 +1,4 @@
-const CIRCLE_RADIUS = 3; // The radius  of the circle
+const CIRCLE_RADIUS = 10; // The radius  of the circle
 
 const insidePoint = (compare) => (point) =>
   (compare.x - point.x) ** 2 + (compare.y - point.y) ** 2 <= CIRCLE_RADIUS ** 2;
