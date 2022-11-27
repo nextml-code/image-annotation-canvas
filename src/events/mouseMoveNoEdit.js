bf4a2849f9f2a4b452b07cafd1b6ccc31ba9ec09
FelixAbrahamsson <felixab@kth.se>
2022-11-27 10:14:59 +0100

    improve: clamp the offset based on zoom

--- src/events/mouseMoveNoEdit.js
+++ src/events/mouseMoveNoEdit.js
@@ -6,15 +6,26 @@ const mouseMoveNoEdit = (state, dispatch, event) => {
     dispatch({
       type: SET_CANVAS_OFFSET,
       payload: {
-        x:
+        x: clampedCanvasOffset(
           getEventLocation(event).x / state.canvasZoom -
-          state.canvasDragStart.x,
-        y:
+            state.canvasDragStart.x,
+          state.canvasDimensions.width,
+          state.canvasZoom,
+        ),
+        y: clampedCanvasOffset(
           getEventLocation(event).y / state.canvasZoom -
-          state.canvasDragStart.y,
+            state.canvasDragStart.y,
+          state.canvasDimensions.height,
+          state.canvasZoom,
+        ),
       },
     });
   }
+};
+
+const clampedCanvasOffset = (offset, dimension, zoom) => {
+  const maxOffset = (dimension - dimension / zoom) / 2;
+  return Math.max(Math.min(offset, maxOffset), -maxOffset);
 };
 
 export default mouseMoveNoEdit;
